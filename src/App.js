52723dfe8a88e3bd0dae62e2960acd06ed939025
ChengDakang <715167986@qq.com>
2020-10-12 15:06:01 +0800

    transitionGroup demo

--- src/App.js
+++ src/App.js
@@ -1,30 +1,66 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import  { CSSTransition } from 'react-transition-group'
+import  { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 class App extends Component {
   constructor(props) {
     super()
     this.state = {
+      list: [],
       show: true
     }
   }
   render() {
     return (
       <>
-        <CSSTransition 
+        {/* <CSSTransition 
           in={this.state.show}
           timeout={1000}
           classNames='fade'
+          unmountOnExit
+          onEntered={(el) => {
+            el.style.color = 'blue'
+          }}
+          appear={true}
         >
           <div>
             hello world
           </div>
         </CSSTransition>
-        <button onClick={this.handleToggole}>toggole</button>
+        <button onClick={this.handleToggole}>toggole</button> */}
+        <TransitionGroup>
+          {
+            this.state.list.map((item, index) => {
+              return (
+                <CSSTransition 
+                  in={this.state.show}
+                  timeout={1000}
+                  classNames='fade'
+                  unmountOnExit
+                  onEntered={(el) => {
+                    el.style.color = 'blue'
+                  }}
+                  appear={true}
+                  key={index}
+                >
+                  <div>{item}</div>
+                </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
+        <button onClick={this.handleAddItem}>toggole</button>
       </>
     )
+  }
+
+  handleAddItem = () => {
+    this.setState(prevState => {
+      return {
+        list: [...prevState.list, 'item']
+      }
+    })
   }
 
   handleToggole = () => {
